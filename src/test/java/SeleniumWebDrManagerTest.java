6cb696d7bb8b4ed98994b9a86f501da10ba3b023
Indu.Aggarwal <induaggarwa@gmail.com>
2021-10-04 17:48:08 +0100

    Code includes WebDriver Manager

---
+++ src/test/java/SeleniumWebDrManagerTest.java
@@ -0,0 +1,48 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class SeleniumWebDrManagerTest {
+    WebDriver driver;
+    FileInputStream fis;
+    Properties props;
+
+    @BeforeTest
+    public void fileLoad() throws IOException {
+        fis = new FileInputStream("src/Resources/Properties/Selenium.property");
+        props = new Properties();
+        props.load(fis);
+    }
+
+    @Test
+    public void seleniumTest1(){
+        String myDriver = props.getProperty("browser");
+
+        if(myDriver.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if(myDriver.equalsIgnoreCase("firefox")){
+             WebDriverManager.firefoxdriver().setup();
+             driver = new FirefoxDriver();
+        }
+        else if(myDriver.equalsIgnoreCase("edgeDriver")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        driver.get(props.getProperty("URL"));
+        System.out.println(driver.getTitle());
+        driver.close();
+
+    }
+
+}
